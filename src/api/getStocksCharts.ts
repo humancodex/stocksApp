1eb9287520fca82d4d91bb4ddf3f03bb57cc669e
humancodex <juansebcarv@outlook.com>
2024-09-27 09:45:09 -0300

    chore: Add fetchData function to get stock charts data from API

---
+++ src/api/getStocksCharts.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+export type DataItem = {
+  datetime: string;
+  close: string;
+};
+
+export const fetchData = async (
+  symbol: string,
+  interval: string ,
+  start_date: string = '2024-09-01',
+  end_date: string = '2024-09-30',
+): Promise<{ x: number; y: number }[]> => {
+  try {
+    const response = await axios.get('https://api.twelvedata.com/time_series', {
+      params: {
+        symbol,
+        interval,
+        start_date,
+        end_date,
+        apikey: process.env.TWELVEDATA_API_KEY,
+      },
+    });
+
+    if (response.data && response.data.values) {
+      return response.data.values.map((item: DataItem) => ({
+        x: new Date(item.datetime).getTime(),
+        y: Number(item.close),
+      }));
+    } else {
+      throw new Error('Invalid or empty response data');
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
